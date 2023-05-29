fa89e5cb98c751beb9ca7106250d84b049f18431
jeemee <jeemeesakariya71886@gmail.com>
2023-05-29 11:48:45 +0530

    half

--- lib/listview1.dart
+++ lib/listview1.dart
@@ -1,29 +1,16 @@
-//import 'dart:ffi';
-//import 'dart:math';
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
-//import 'login.dart';
 import 'loginpage1.dart';
-//import 'package:pu_resourcse/login.dart';
-//import 'package:flutter/material.dart;;;
-
 
 class Branches extends StatefulWidget {
   const Branches({super.key, });
 
-
-
   @override
   State<Branches> createState() => _BranchesState();
-
 
 }
 
 class _BranchesState extends State<Branches>{
-
-
 
   @override
   Widget build(BuildContext context) {
